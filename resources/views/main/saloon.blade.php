f60691033ba6ac6c10b3ef43d585bd8b3c3f6d33
Kamil Drozd <drozdk@centrum.cz>
2020-05-31 02:15:58 +0200

    saloon page added

--- resources/views/main/saloon.blade.php
+++ resources/views/main/saloon.blade.php
@@ -21,6 +21,32 @@
   </div>
   <div class="services">
     <h1>Služby a ceník</h1>
+    @foreach ($stylists as $stylist)
+      <div class="stylisttreatment">
+        <div class="stylistbox">
+          <img src="{{ asset('/images/stylists/') }}/{{$stylist->profile_photo_url}}" alt="Photo {{ $stylist->user->first_name .' '. $stylist->user->last_name }}" class="stylistbox__image">
+          <p> <strong>{{ $stylist->user->first_name .' '. $stylist->user->last_name }}</strong><br>
+              {{-- {{ $stylist->job_title }} <br> --}}
+              {{ $stylist->service }} <br>
+          </p>
+        </div>
+        <div class="treatmentbox">
+          <table>
+            @foreach ($stylist->treatments as $treatment)
+            <tr>
+              <td class="treatmentname">
+                {{ $treatment->name }}
+              </td>
+              <td class="treatmentprice">
+                {{ $treatment->price }} Kč
+              </td>
+            </tr>    
+            @endforeach  
+          </table>
+        </div>
+      </div>    
+    @endforeach
+    
   </div>
 
   
